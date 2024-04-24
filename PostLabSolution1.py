6f9a1dd03d7824e4cbd38e479fe25132f84f22de
TorresNA <151115842+n1coleabt@users.noreply.github.com>
2024-04-25 02:33:59 +0800

    Update PostLabSolution1.py

--- PostLabSolution1.py
+++ PostLabSolution1.py
@@ -1,1 +1,25 @@
+class Student(object):
+    def __init__(self, name, number):
+        self.name = name
 
+    def getName(self):
+        return self.name
+
+    def __eq__(self, student):
+        return self.name == student.name 
+
+    def __lt__(self, student):
+        return self.name < student.name 
+
+    def __gt__(self, student):
+        return self.name > student.name or self.name == student.name
+
+def main():
+    s1 = Student("Taylor", 10)
+    s2 = Student("Brent", 20)
+    print(s1.getName() + " is equal to " + s2.getName() + " = " + str(s1.__eq__(s2)))
+    print(s1.getName() + " is less than " + s2.getName() + " = " + str(s1.__lt__(s2)))
+    print(s1.getName() + " is greater than or equal to " + s2.getName() + " = " + str(s1.__gt__(s2)))
+
+if __name__ == "__main__":
+ main()
